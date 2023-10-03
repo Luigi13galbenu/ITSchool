ad689d110e89023fd5230e4d31d04c5fe45f3151
Luigi <luigi13galbenu@gmail.com>
2023-10-02 18:55:18 -0700

    session4

---
+++ src/session4/challenges/Challenge4.java
@@ -0,0 +1,13 @@
+package session4.challenges;
+
+//  4. Write a Java program where you declare two integer variables heightOne and heightTwo with any values. Use a ternary
+//     operator to determine the greater height and assign it to a variable maximumHeight. Print the result.
+
+public class Challenge4 {
+    public static void main(String[] args) {
+        int heightOne = 20;
+        int heightTwo = 25;
+        int maximumHeight = heightOne>heightTwo ? heightOne: heightTwo;
+        System.out.println(maximumHeight);
+    }
+}
