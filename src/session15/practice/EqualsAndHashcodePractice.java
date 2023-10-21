8f168ce5faeceffa47e926d6cfce044a477b80fb
Luigi <luigi13galbenu@gmail.com>
2023-10-20 21:26:03 -0700

    session15

---
+++ src/session15/practice/EqualsAndHashcodePractice.java
@@ -0,0 +1,37 @@
+package session15.practice;
+
+import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EqualsAndHashcodePractice {
+
+    public EqualsAndHashcodePractice() {
+    }
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.setId(1);
+        employee1.setFirstName("Alice");
+        employee1.setLastName("Bond");
+
+        Employee employee2 = new Employee();
+        employee2.setId(1);
+        employee2.setFirstName("Alice");
+        employee2.setLastName("Bond");
+
+        System.out.println("employee1 equals employee2: " + employee1.equals(employee2));
+        System.out.println("employee1 hascode:" + employee1.hashCode());
+        System.out.println("employee2 hascode:" + employee2.hashCode());
+
+        Set<Employee> employeeSet = new HashSet<>();
+        employeeSet.add(employee1);
+        employeeSet.add(employee2);
+
+        System.out.println("Set size: " + employeeSet.size());
+
+        System.out.println("Employee1: " + employee1);
+
+
+    }
+}
